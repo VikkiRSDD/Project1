51673bc6ed1026e955275e7669e8db19aa487695
VikkiRSDD <vigneshdmech555@gmail.com>
2023-03-02 15:41:47 +0530

    B branch

--- src/org/test/PrintProgram.java
+++ src/org/test/PrintProgram.java
@@ -3,5 +3,6 @@ package org.test;
 public class PrintProgram {
 	public static void main(String[] args) {
 		System.out.println("simple java project");
+		System.out.println("Rex code added");
 	}
 }
