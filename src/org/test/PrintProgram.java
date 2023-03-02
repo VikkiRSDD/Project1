0a70a386a8b0d19565c87bb7f90a8e31fbec8d98
VikkiRSDD <vigneshdmech555@gmail.com>
2023-03-02 12:07:18 +0530

    new lines added

--- src/org/test/PrintProgram.java
+++ src/org/test/PrintProgram.java
@@ -3,5 +3,7 @@ package org.test;
 public class PrintProgram {
 	public static void main(String[] args) {
 		System.out.println("simple java project");
+		System.out.println("update code to master");
+		
 	}
 }
